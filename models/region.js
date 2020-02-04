6e93d454a59a24545c60a41b1497693143eb1960
Gildas Cherruel <gildas@breizh.org>
2020-02-05 00:16:13 +0900

    Wrong property name

--- models/region.js
+++ models/region.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const RegionSchema = new mongoose.Schema({
   RegionID:   { type: Number, unique: true },
-  RegionName: { type: String, unique: true },
+  RegionDescription: { type: String, unique: true },
 })
 
 RegionSchema.query.byName = function(name) { return this.where({ RegionName: new RegExp(name, 'i') }) }
